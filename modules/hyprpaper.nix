eec3dc642803cd1872d30abcc498c2fff9aec85d
nick <nicc.gemm@gmail.com>
2026-01-19 14:58:56 +0100

    fix wallpaper

--- modules/hyprpaper.nix
+++ modules/hyprpaper.nix
@@ -2,12 +2,6 @@
 
 let
   fileName = name: "${config.home.homeDirectory}/Wallpapers/" + name;
-  # pool = fileName "oo8dehbnf3ae1.jpeg";
-  # bed = fileName "4o4uaw69k6vf1.jpeg";
-  # bath = fileName "qy128h3jhh9e1.jpeg";
-  # carpet = fileName "qqcc1q1x6axe1.jpeg";
-  # collage-big = fileName "d172qww3485d1.png";
-  # collage-small = fileName "tqwj8joa6c8d1.png";
   silksong = fileName "silksong";
 
   # set-wallpaper = pkgs.writeShellScriptBin "set-wallpaper" ''
@@ -33,19 +27,10 @@ in {
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [
-        # pool
-        # bed
-        # bath
-        # carpet
-        # collage-big
-        # collage-small
-        silksong
-      ];
-
-      wallpaper = [
-        ", ${silksong}"
-      ];
+      wallpaper = {
+        monitor = "";
+        path = silksong;
+      };
     };
   };
 }
