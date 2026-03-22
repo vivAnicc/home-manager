e40907bf6ec81e2455e4c6609a99a5cb33128629
nick <nicc.gemm@gmail.com>
2026-03-22 20:46:01 +0100

    Add desktop entries to make Amethyst the default handler of nxm links

--- modules/games.nix
+++ modules/games.nix
@@ -9,4 +9,27 @@
 
     inputs.amethyst.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
+
+  xdg.desktopEntries = {
+    amethystmodmanager = {
+      type = "Application";
+      name = "Amethyst Mod Manager";
+      exec = "amethyst %U";
+      terminal = false;
+      mimeType = [
+        "x-scheme-handler/nxm"
+      ];
+    };
+
+    amethystmodmanager-nxm = {
+      type = "Application";
+      name = "Amethyst Mod Manager (NXM Handler)";
+      exec = "amethyst --nxm %U";
+      terminal = false;
+      noDisplay = true;
+      mimeType = [
+        "x-scheme-handler/nxm"
+      ];
+    };
+  };
 }
