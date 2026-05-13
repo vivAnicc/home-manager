74707d074788580567c5017b87c7936454f438f4
nick <nicc.gemm@gmail.com>
2026-05-13 20:07:37 +0200

    Update flake.lock

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -6,7 +6,7 @@
     gpu.nvidia = {
       enable = true;
       version = "595.71.05";
-      sha256 = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
+      sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
     };
     # nixGL = {
     #   packages = inputs.nixgl.packages;
